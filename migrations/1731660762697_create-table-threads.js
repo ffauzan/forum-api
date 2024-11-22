297aa3a7f18e3f7d6bf97c22fece241003b68677
ffauzan <f.fauzan@outlook.com>
2024-11-22 09:06:58 +0700

    Create table threads & comments

---
+++ migrations/1731660762697_create-table-threads.js
@@ -0,0 +1,34 @@
+exports.up = (pgm) => {
+  pgm.createTable('threads', {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+    },
+    user_id: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+      references: 'users',
+      onDelete: 'cascade',
+    },
+    title: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    body: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    created_at: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    updated_at: {
+      type: 'TEXT',
+      notNull: true,
+    },
+  });
+};
+
+exports.down = (pgm) => {
+  pgm.dropTable('threads');
+};
---
+++ migrations/1731661803533_create-table-comments.js
@@ -0,0 +1,34 @@
+exports.up = (pgm) => {
+  pgm.createTable('comments', {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+    },
+    thread_id: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+      references: 'threads',
+      onDelete: 'cascade',
+    },
+    reply_to: {
+      type: 'VARCHAR(50)',
+      references: 'comments',
+      onDelete: 'cascade',
+      notNull: false,
+    },
+    content: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    created_at: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    updated_at: {
+      type: 'TEXT',
+      notNull: true,
+    },
+  });
+};
+
+exports.down = (pgm) => {};
