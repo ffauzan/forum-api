83cfd3b5029a89a638c31cec3ac465b011e39bcc
ffauzan <f.fauzan@outlook.com>
2024-11-27 16:37:20 +0700

    Refactor GetThreadUseCase tests to use entity classes for AddedThread, RegisteredUser, and AddedComment

--- src/Applications/use_case/threads/_test/GetThreadUseCase.test.js
+++ src/Applications/use_case/threads/_test/GetThreadUseCase.test.js
@@ -1,7 +1,10 @@
+const AddedThread = require('../../../../Domains/threads/entities/AddedThread');
 const GetThreadUseCase = require('../GetThreadUseCase');
 const ThreadRepository = require('../../../../Domains/threads/ThreadRepository');
 const UserRepository = require('../../../../Domains/users/UserRepository');
 const CommentRepository = require('../../../../Domains/comments/CommentRepository');
+const RegisteredUser = require('../../../../Domains/users/entities/RegisteredUser');
+const AddedComment = require('../../../../Domains/comments/entities/AddedComment');
 
 describe('GetThreadUseCase', () => {
   it('should orchestrate the get thread action correctly', async () => {
@@ -34,62 +37,69 @@ describe('GetThreadUseCase', () => {
 
     // Mocking needed function
     mockThreadRepository.getThreadById = jest.fn()
-      .mockImplementation(() => Promise.resolve({
+      .mockImplementation(() => Promise.resolve(new AddedThread({
         id: 'thread-123',
         userId: 'user-123',
         title: 'Thread Title',
         body: 'Thread body content',
         createdAt: '2024-11-26T00:00:00.000Z',
-      }));
+      })));
 
     mockUserRepository.getUserById = jest.fn()
       // Thread owner
-      .mockImplementationOnce(() => Promise.resolve({
+      .mockImplementationOnce(() => Promise.resolve(new RegisteredUser({
         id: 'user-123',
         username: 'threadOwner',
-      }))
+        fullname: 'Thread Owner',
+      })))
       // Comment 1 user
-      .mockImplementationOnce(() => Promise.resolve({
+      .mockImplementationOnce(() => Promise.resolve(new RegisteredUser({
         id: 'user-456',
         username: 'commentUser',
-      }))
+        fullname: 'Comment User',
+      })))
       // Reply of comment 1 user
-      .mockImplementationOnce(() => Promise.resolve({
+      .mockImplementationOnce(() => Promise.resolve(new RegisteredUser({
         id: 'user-123',
         username: 'threadOwner',
-      }))
+        fullname: 'Thread Owner',
+      })))
       // Comment 2 user
-      .mockImplementationOnce(() => Promise.resolve({
+      .mockImplementationOnce(() => Promise.resolve(new RegisteredUser({
         id: 'user-456',
         username: 'commentUser',
-      }));
+        fullname: 'Comment User',
+      })));
 
     mockCommentRepository.getCommentsByThreadId = jest.fn()
-      .mockImplementation(() => Promise.resolve([
-        {
+      .mockImplementation((threadId) => Promise.resolve([
+        new AddedComment({
           id: 'comment-123',
           userId: 'user-456',
+          threadId,
           content: 'Comment content',
           createdAt: '2024-11-26T01:00:00.000Z',
           replyTo: null,
           isDeleted: false,
-        },
-        {
+        }),
+        new AddedComment({
           id: 'comment-124',
           userId: 'user-123',
+          threadId,
           content: 'Reply to comment content',
           createdAt: '2024-11-26T02:00:00.000Z',
           replyTo: 'comment-123',
           isDeleted: true,
-        },
-        {
+        }),
+        new AddedComment({
           id: 'comment-125',
           userId: 'user-456',
+          threadId,
           content: 'Comment content 2',
           createdAt: '2024-11-26T03:00:00.000Z',
           replyTo: null,
           isDeleted: true,
-        },
+        }),
       ]));
 
     // Creating instance of use case
