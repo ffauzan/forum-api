404ad60f24186021bab834b477777c656cef7200
ffauzan <f.fauzan@outlook.com>
2024-12-24 13:43:31 +0700

    Modif hello world

--- src/Infrastructures/http/createServer.js
+++ src/Infrastructures/http/createServer.js
@@ -125,7 +125,7 @@ const createServer = async (container) => {
     method: 'GET',
     path: '/',
     handler: () => ({
-      value: 'Hello world!',
+      value: 'Hello world 2',
     }),
   });
 
